a25af32995230b28818a5e12191f28ed2bb61491
samsuddeen <samsuddeenaalam7@gmail.com>
2025-05-22 19:46:35 +0545

    added and fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,23 +147,30 @@
         }
     });
 
+    // var progressPath = document.querySelector('.back-to-top-wrap path');
     var progressPath = document.querySelector('.back-to-top-wrap path');
+
+if (progressPath) {
     var pathLength = progressPath.getTotalLength();
     progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
     progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
     progressPath.style.strokeDashoffset = pathLength;
     progressPath.getBoundingClientRect();
     progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+
     var updateProgress = function () {
         var scroll = $(window).scrollTop();
         var height = $(document).height() - $(window).height();
         var progress = pathLength - (scroll * pathLength / height);
         progressPath.style.strokeDashoffset = progress;
-    }
+    };
+
     updateProgress();
     $(window).scroll(updateProgress);
+
     var offset = 50;
     var duration = 550;
+
     jQuery(window).on('scroll', function () {
         if (jQuery(this).scrollTop() > offset) {
             jQuery('.back-to-top-wrap').addClass('active-progress');
@@ -171,11 +178,15 @@
             jQuery('.back-to-top-wrap').removeClass('active-progress');
         }
     });
+
     jQuery('.back-to-top-wrap').on('click', function (event) {
         event.preventDefault();
         jQuery('html, body').animate({ scrollTop: 0 }, duration);
         return false;
-    })
+    });
+}
+
+
 
     /* Date */
     var date = new Date().getFullYear();
